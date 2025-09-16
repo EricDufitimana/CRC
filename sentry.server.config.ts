63fcf3c939210f6e302d395929b3c911af7053ee
Coding <coding@ASYV-IT-LBPC137.lnfhs.asyv.org>
2025-09-16 17:34:42 +0200

    Fixed Zenith Calendar Error

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://31eaf9ab62af2514c518332ab0205092@o4510029100613632.ingest.de.sentry.io/4510029999702096",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
