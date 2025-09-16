6ddc03ead186f1345f938fce09822d9f206d5201
Coding <coding@ASYV-IT-LBPC137.lnfhs.asyv.org>
2025-09-16 17:18:19 +0200

    Fixed Tooltip provider error

--- zenith/src/App.tsx
+++ zenith/src/App.tsx
@@ -1,4 +1,3 @@
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -12,7 +11,6 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
@@ -24,7 +22,6 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-    </TooltipProvider>
   </QueryClientProvider>
 );
 
